6a9ecd936529e8ea0ab9c3b3025a918335c30c6a
warre-rgb <warre@denheuvel.be>
2025-11-25 11:32:56 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,9 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    knuffel += 5
+})
+input.onButtonPressed(Button.B, function () {
+    voeding += 5
+})
+let dood = 0
+let knuffel = 20
+let voeding = 20
+loops.everyInterval(500, function () {
+    knuffel += -1
+    voeding += -1
+})
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        let knuffel = 0
-        let voeding = 0
-        if (voeding < 10 && knuffel < 10) {
-            basic.showIcon(IconNames.Happy)
-        }
+    if (knuffel > 10 && voeding > 10) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (voeding < 10 && voeding > dood || knuffel < dood && voeding < dood) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (true) {
+        knuffel += 0
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
     }
 })
